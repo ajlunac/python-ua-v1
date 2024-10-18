a320334c03c12e0285cd0f317176f81572fc1241
Javier Luna Castellon <lunaion2200@gmail.com>
2024-10-17 23:24:33 -0500

    Primer renglon de la calculadora

--- src/calculadora.py
+++ src/calculadora.py
@@ -27,7 +27,31 @@ class Calculadora(tk.Tk):
         entrada = tk.Entry(entrada_frame, font=('Arial', 18, 'bold'), textvariable=self.entrada_texto, width=30, justify=tk.RIGHT)
         entrada.grid(row=0, column=0, ipady=10)
         
+        # Agregamos un segundo frame para la parte inferior.
+        botones_frame = tk.Frame(self, width=400, height=450, bg='grey')
+        botones_frame.pack()
         
+        
+        # Primer renglón.
+        # Botón de Limpiar.
+        boton_limpiar = tk.Button(botones_frame, text='C', width=36, height=3, 
+                                  bd=0, bg='#eee', cursor='hand2', command=self._evento_limpiar)
+        boton_limpiar.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+        
+        # Botón de dividir.
+        boton_dividir = tk.Button(botones_frame, text='/', width=18, height=3, 
+                                  bd=0, bg='#eee', cursor='hand2', command=lambda: self._evento_click('/'))
+        boton_dividir.grid(row=0, column=3, padx=1, pady=1)
+        
+    
+    def _evento_limpiar(self):
+        self.expresion = ''
+        self.entrada_texto.set(self.expresion)
+        
+    def _evento_click(self, elemento):
+        # Concatenamos el nuevo elemento a la expresión ya existente.
+        self.expresion = f'{self.expresion}{elemento}'
+        self.entrada_texto.set(self.expresion)
         
         
 if __name__ == '__main__':
